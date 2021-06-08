630d7a840885e2acae98953e4d39bc4380fe5bd0
Inder Panesar <Inder_Panesar@hotmail.com>
2021-06-08 12:07:47 +0100

    Add google navbar to app

--- aston_math_application/lib/ui/screens/home/home_page.dart
+++ aston_math_application/lib/ui/screens/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,36 +7,94 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int selectedPage = 0;
+  int _selectedIndex = 0;
 
   final _pageOptions = [
-    Container(),
-    Container(),
-    Container()
+    Container(color: Colors.red,),
+    Container(color: Colors.white,),
+    Container(color: Colors.lightBlue,),
+    Container(color: Colors.lightGreen,)
   ];
+
+  static const TextStyle optionStyle =  TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Likes',
+      style: optionStyle,
+    ),
+    Text(
+      'Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
         backgroundColor: Colors.white,
-        body: _pageOptions[selectedPage],
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home, size: 30), label: '1st'),
-            BottomNavigationBarItem(icon: Icon(Icons.mail, size: 30), label: '2nd'),
-            BottomNavigationBarItem(icon: Icon(Icons.account_circle, size: 30), label: '3rd'),
-          ],
-          selectedItemColor: Colors.green,
-          elevation: 5.0,
-          unselectedItemColor: Colors.green[900],
-          currentIndex: selectedPage,
-          backgroundColor: Colors.white,
-          onTap: (index){
-            setState(() {
-              selectedPage = index;
-            });
-          },
-        )
+        body: _pageOptions[_selectedIndex],
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.1),
+              )
+            ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+              child: GNav(
+                rippleColor: Colors.grey[300]!,
+                hoverColor: Colors.grey[100]!,
+                gap: 8,
+                activeColor: Colors.black,
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.grey[100]!,
+                color: Colors.black,
+                tabs: [
+                  GButton(
+                    icon: Icons.home_rounded,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: Icons.line_style_rounded,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: Icons.search_rounded,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: Icons.ac_unit,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+               },
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
